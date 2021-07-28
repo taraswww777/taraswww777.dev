2f23b57277ff3e4913ba042d55bf1c3e0f2a383c
Taras_Kovalev <taraswww777@mail.ru>
2021-07-28 21:10:48 +0300

    Make icons Issue: 3

---
+++ site/wp-content/themes/brushed/nextjs/pages/components/icon/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {ComponentProps} from '../../types/common';
+import styles from './icon.module.scss';
+import {IconType} from './types';
+
+export type IconProps = ComponentProps & {
+    icon: IconType,
+}
+
+export const Icon = ({icon}: IconProps) => (
+    <span className={[styles[icon], styles.icon].join(' ')} />
+);
