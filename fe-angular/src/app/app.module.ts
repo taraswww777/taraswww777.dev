7c445ab9138e105374abaed216666862235ab203
taraswww777 <taraswww777@mail.ru>
2022-08-28 18:08:43 +0300

    about-me

--- fe-angular/src/app/app.module.ts
+++ fe-angular/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ContactsComponent } from './contacts/contacts.component';
+import { AppComponent } from './components/app/app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ContactsComponent
+    ContactsComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
