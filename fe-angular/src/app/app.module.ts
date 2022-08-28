9a107fb2207b6acde6fd20af90f7fbc5d486ec72
taraswww777 <taraswww777@mail.ru>
2022-08-28 14:39:50 +0300

    add support fontawesome

--- fe-angular/src/app/app.module.ts
+++ fe-angular/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ContactsComponent } from './contacts/contacts.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- fe-angular/src/app/contacts/contacts.component.ts
+++ fe-angular/src/app/contacts/contacts.component.ts
@@ -1,4 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {faWhatsapp, faTelegram, faGithub} from '@fortawesome/free-brands-svg-icons';
+import {faPhone, faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import {IconProp} from '@fortawesome/fontawesome-svg-core';
+
+interface Contact {
+  href: string,
+  code: string,
+  text: string,
+  icon: IconProp
+}
+
+const CONTACTS: Contact[] = [
+  {href: 'https://t.me/taraswww777/', code: 'telegram', text: '@taraswww777', icon: faTelegram},
+  {href: 'tel:+79246736538', text: '+7-924-673-65-38', code: 'phone', icon: faPhone},
+  {href: 'mailto:taraswww777@mail.ru', text: 'taraswww777@mail.ru', code: 'email', icon: faEnvelope},
+  {href: 'https://github.com/taraswww777', text: 'taraswww777', code: 'github', icon: faGithub},
+];
 
 @Component({
   selector: 'app-contacts',
@@ -6,8 +23,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacts.component.scss']
 })
 export class ContactsComponent implements OnInit {
+  faWhatsapp = faWhatsapp
+  faTelegram = faTelegram
+  faPhone = faPhone
 
-  constructor() { }
+  contacts: Contact[] = CONTACTS;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
