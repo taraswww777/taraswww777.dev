3ad4365d213d32ede960b76ca683c2732f9f40a2
taraswww777 <taraswww777@mail.ru>
2023-04-16 00:52:21 +0300

    #45 AboutMeComponent

--- src/components/about-me/about-me.component.tsx
+++ src/components/about-me/about-me.component.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import {imageLoader} from 'src/utils/imageLoader';
 import ImgSmall from '../../public/assets/my-photo-small.jpg'
+import {Grid, List, Title, Text} from '@mantine/core';
+import React from 'react';
 
 export const AboutMeComponent = () => {
   const date = new Date();
@@ -10,41 +12,38 @@ export const AboutMeComponent = () => {
   const age = Math.floor(timeDiff / (1000 * 3600 * 24 * 365))
 
   return (
-    <div className="row">
-      <picture className="col-md-4 col-sm-12">
+    <Grid>
+      <Grid.Col xs={12} md={4}>
         <Image
           src={ImgSmall}
           className="img-fluid"
-          width="348"
-          height="521"
+          style={{maxWidth: '100%', height: 'auto'}}
           alt="Это я"
           loader={imageLoader}
         />
-      </picture>
-      <div className="col-md-8 col-sm-12">
-        <div className="mt-3">
-          <h2>Всем, привет!</h2>
-          <p>Меня зовут Тарас.</p>
-          <p>
-            Если вы попали на этот сайт осмысленно,
-            то наверняка вас интересуют факты обо мне,
-            я это знал и заранее их описал ниже.
-          </p>
-        </div>
-        <p>Факты обо мне:</p>
-        <ul>
-          <li>Родился 1 марта 1994г ({age}лет)</li>
-          <li>
-            В 2015 получил степень бакалавра "Бизнес-информатики" в
-            <a href="https://www.amursu.ru/" target="_blank">АмГУ</a>
-          </li>
-          <li>В 2013 начал совмещать работу программистом и учёбу в университете</li>
-          <li>Начиная с сентября 2015 на постоянной основе начал работать программистом на коммерческой основе</li>
-          <li>С 2019 начал заниматься менторством по програмированию</li>
-          <li>В настоящее время работаю FE разработчиком</li>
-          <li>Опыт работы и частичное портфолио расположено ниже.</li>
-        </ul>
-      </div>
-    </div>
+      </Grid.Col>
+      <Grid.Col xs={12} md={8}>
+        <Title order={2}>Всем, привет!</Title>
+        <Text my={'1rem'}>Меня зовут Тарас.</Text>
+        <Text>
+          Если вы попали на этот сайт осмысленно,
+          то наверняка вас интересуют факты обо мне,
+          я это знал и заранее их описал ниже.
+        </Text>
+        <Text my={'1rem'}>Факты обо мне:</Text>
+        <List listStyleType="disc">
+          <List.Item>Родился 1 марта 1994г ({age}лет)</List.Item>
+          <List.Item>
+            В 2015 получил степень бакалавра "Бизнес-информатики"
+            в&nbsp;<a href="https://www.amursu.ru/" target="_blank">АмГУ</a>
+          </List.Item>
+          <List.Item>В 2013 начал совмещать работу программистом и учёбу в университете</List.Item>
+          <List.Item>Начиная с сентября 2015 на постоянной основе начал работать программистом на коммерческой основе</List.Item>
+          <List.Item>С 2019 начал заниматься менторством по програмированию</List.Item>
+          <List.Item>В настоящее время работаю FE разработчиком</List.Item>
+          <List.Item>Опыт работы и частичное портфолио расположено ниже.</List.Item>
+        </List>
+      </Grid.Col>
+    </Grid>
   );
 };
