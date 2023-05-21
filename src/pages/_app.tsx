da837208b9c8d8fe7a980e4e8ecda988bdce809c
taraswww777 <taraswww777@mail.ru>
2023-05-22 00:06:13 +0300

    add meta name zen-verification and fix Head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '../styles/globals.scss'
 import type {AppProps} from 'next/app'
+import Head from 'next/head'
 
 export default function App({Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>taraswww777.dev</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta charSet='utf-8' />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="zen-verification" content="z0wd2SS2t0DrS2qFsQEG5fIHme88fCZEm4V3N4nuwewvhhKFLkWCuVjwFYJeFIaG" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,13 +5,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <title>taraswww777.dev</title>
         <script src="https://kit.fontawesome.com/45f9b38c9b.js" crossOrigin="anonymous"></script>
       </Head>
       <body>
       <Main/>
       <NextScript/>
-
       <YaMetric/>
       </body>
     </Html>
