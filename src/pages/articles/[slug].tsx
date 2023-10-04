cd34e1fa3ef5cc943e95860c5bab4b5fef9e0392
taraswww777 <taraswww777@mail.ru>
2023-10-04 21:03:33 +0300

    apply mdx-js

--- src/pages/articles/[slug].tsx
+++ src/pages/articles/[slug].tsx
@@ -1,7 +1,18 @@
 import {PageTemplates} from '../../components/PageTemplates';
 import Head from 'next/head';
+import {useRouter} from 'next/router';
+import Page from './_page.mdx';
+import {MDXProvider} from '@mdx-js/react';
 
-export const Articles = () => {
+interface RouteParams {
+  slug: string
+}
+
+const Articles = () => {
+  const router = useRouter();
+  const {slug} = router.query
+
+  console.log('slug:', slug)
   return (
     <PageTemplates>
       <Head>
@@ -13,9 +24,15 @@ export const Articles = () => {
             <h1 className="h2">Статьи</h1>
           </div>
           <div className="col-12">
+            <MDXProvider>
+              <Page />
+            </MDXProvider>
+            slug: {slug}
           </div>
         </div>
       </div>
     </PageTemplates>
   )
 }
+
+export default Articles;
