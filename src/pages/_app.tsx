40339174912120f3646eb5c4be7f25d1cc76e010
taraswww777 <taraswww777@mail.ru>
2023-10-10 06:23:36 +0300

    fix order meta char set utf-8

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,9 +8,9 @@ export default function App({Component, pageProps}: AppProps) {
   return (
     <MDXProvider>
       <Head>
+        <meta charSet="utf-8" />
         <title>{SITE_NAME}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charSet='utf-8' />
         <link rel="icon" href="/favicon.ico" />
         <meta name="zen-verification" content="z0wd2SS2t0DrS2qFsQEG5fIHme88fCZEm4V3N4nuwewvhhKFLkWCuVjwFYJeFIaG" />
       </Head>
