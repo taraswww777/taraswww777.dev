33f7334285a4e2bbc712a1275dc0ec8952a12e92
taraswww777 <taraswww777@mail.ru>
2023-12-12 13:53:06 +0300

    fix build

--- src/resources/work-experience.ts
+++
@@ -1,9 +0,0 @@
-import {WorkExperienceDto} from 'src/types/dto';
-import {dataWorkExperience} from "../__data";
-
-
-export const loadWorkExperience = (): WorkExperienceDto[] => (
-  dataWorkExperience.sort((a, b) => (
-    new Date(b.dateBegin).getTime() - new Date(a.dateBegin).getTime()
-  ))
-);
