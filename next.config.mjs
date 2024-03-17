026b162c608cb76a8763d6908ac1b71e710c2ba6
taraswww777 <taraswww777@mail.ru>
2024-03-17 18:00:58 +0300

    #70 fix build

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,12 @@ import nextMdx from '@next/mdx';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    unoptimized: true
+  },
   // Support MDX files as pages:
   pageExtensions: ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js'],
 }
--- scripts/sitemap-generator.js
+++ scripts/sitemap-generator.js
@@ -2,10 +2,19 @@ const sitemap = require('nextjs-sitemap-generator');
 const path = require('path');
 
 sitemap({
-  baseUrl: "https://taraswww777.dev/",
+  baseUrl: "https://taraswww777.dev",
   pagesDirectory: path.resolve(__dirname, '../out/'),
   targetDirectory: path.resolve(__dirname, '../out/'),
-  ignoredExtensions: ["js", "map", "json", "xml", "png", "jpg", "jpeg", "svg"],
-  ignoredPaths: ["[fallback]"],
+  ignoredExtensions: [
+    "js", "map", "json", "xml",
+    "png", "jpg", "jpeg", "svg", 'ico', 'ico',
+    'eot', 'ttf', 'woff', 'woff2',
+  ],
+  ignoredPaths: [
+    // "/fonts",
+    // "/index.html"
+  ],
   allowFileExtensions: true
+}).then(() => {
+  console.log(`✅ sitemap.xml generated!`);
 });
