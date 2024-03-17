267e88dbd57c6e62abc20ff33517418a3642265b
taraswww777 <taraswww777@mail.ru>
2024-03-17 17:20:30 +0300

    #70 apply responsive design

--- src/components/PageTemplate/components/PageHeader.tsx
+++ src/components/PageTemplate/components/PageHeader.tsx
@@ -5,31 +5,35 @@ import Link from 'next/link';
 export const PageHeader = () => {
   return (
     <div className="
-     w-full p-2.5 flex flex-wrap justify-center items-center mx-auto
-     lg:flex-nowrap
-      ">
+    w-full p-2.5 flex flex-wrap justify-center items-center mx-auto
+    md:flex-nowrap
+    ">
       <div className="
-      uppercase w-full text-center
-      lg:w-auto lg:pr-2.5 lg:text-left
+      uppercase grow-1 text-center
+      md:grow-0 md:pr-2.5 md:text-left md:pb-0
       ">
         <Link href="/">taraswww777</Link>
       </div>
+
       <div className="
-      w-full text-xl  text-center
-      lg:pr-2.5
+      text-xl grow-1 text-center w-full hidden
+      md:block
       ">
         <p>
           Ковалёв Тарас Викторович
         </p>
-        <p className="text-xs hidden lg:block ">
+        <p className="
+        text-xs hidden
+        lg:inline-block
+        ">
           Увеличение&nbsp;<FaIcon iconName={'fa-solid fa-arrow-trend-up'} />&nbsp;прибыли&nbsp;и
           сокращение&nbsp;<FaIcon iconName={'fa-solid fa-arrow-trend-down'} />&nbsp;издержек
         </p>
       </div>
 
       <nav className="
-      w-full text-center p-0
-      md:w-auto md:text-right md:pl-2.5
+      grow-1 text-center p-0
+      md:grow-0 md:text-right md:pl-2.5
       ">
         <Menu />
       </nav>
