4bad8e068c1b70c0c7aa2b009086359054277e2c
Taras Kovalev <kovalevtv@vtb.ru>
2024-03-24 15:25:05 +0300

    fix build

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -6,7 +6,7 @@ export const LINKS = {
   articles: {
     tsValueNeedType: {link: '/articles/2024-01-18-ts-value-need-type', title: 'Typescript. Нужно ли явно указывать тип переменной?'},
     includeFontsInWebApp: {link: '/articles/2024-01-13-include-fonts-in-web-app', title: 'Подключение шрифтов в WEB приложении'},
-    resultGitTask: {link: '/articles/2023-12-18-result-git-task', title: 'Комментарии к заданию по GIT от ResultScholl'},
+    // resultGitTask: {link: '/articles/2023-12-18-result-git-task', title: 'Комментарии к заданию по GIT от ResultScholl'},
     decompositionOfComponentsInUi: {link: '/articles/2023-12-13-decomposition-of-components-in-ui', title: 'Декомпозиция компонентов в UI'},
     tsAsConst: {link: '/articles/2023-11-03-ts-as-const', title: 'TS. Какая польза от as const'},
     frontendConfDay0: {link: '/articles/2023-10-01-frontend-conf-day-0', title: 'FrontendConf 2023 день 0'},
